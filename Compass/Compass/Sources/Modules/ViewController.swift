d7b0c8d9ab68faac80d2fefadf3c6aa688d7d213
semaDilthey <gaidsemen@yandex.ru>
2024-04-04 16:31:23 +0300

    1st commit: project setup + file management

---
+++ Compass/Compass/Sources/Modules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compass
+//
+//  Created by Семен Гайдамакин on 04.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

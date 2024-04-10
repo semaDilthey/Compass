fb1a58b70328336ae3c0879c9452449af8474fd4
semaDilthey <gaidsemen@yandex.ru>
2024-04-10 23:18:57 +0300

    Worked on scenedelegate

--- Compass/Compass/Sources/App/SceneDelegate.swift
+++ Compass/Compass/Sources/App/SceneDelegate.swift
@@ -11,13 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let compasVM = CompasViewModel()
         let vc = CompasViewController(viewModel: compasVM)
-        window?.rootViewController = vc
+        let rootVC = BaseNavController(rootViewController: vc)
+        window?.rootViewController = rootVC
         window?.makeKeyAndVisible()
     }
 
